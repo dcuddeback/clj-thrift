72ee9447e8582cbfc8fea05a985bc00b6fbfae13
EricGebhart <e.a.gebhart@gmail.com>
2013-12-23 14:10:38 -0500

    Added tests for field-names and current-field-name

--- test/clj_thrift/type_test.clj
+++ test/clj_thrift/type_test.clj
@@ -12,3 +12,12 @@
   (fact "returns the set of valid field ids for structs"
     (type/field-ids Name) => (just #{1 2})
     (type/field-ids ExampleStruct) => (just #{3 4 5 7})))
+
+(facts "field-names"
+  (fact "returns the set of valid field names for unions"
+    (type/field-names Identity) => (just #{"name" "ssn"})
+    (type/field-names ExampleUnion) => (just #{"foo" "bar" "baz"}))
+
+  (fact "returns the set of valid field ids for structs"
+    (type/field-names Name) => (just #{"firstName" "lastName"})
+    (type/field-names ExampleStruct) => (just #{"foo" "bar" "baz" "qux"})))
